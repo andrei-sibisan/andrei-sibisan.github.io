3a68250442febbca7966f28acd9749f20e5a92ad
Andrei Sibisan <andrei.sibisan@gmail.com>
2022-07-20 13:40:27 +0300

    GIFS SETUP

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ let searchStr = "";
 let hour = document.querySelector(".hour-hand");
 let minute = document.querySelector(".minute-hand");
 let second = document.querySelector(".second-hand");
+
+let hr;
+let min;
+let sec;
 function clock(offsetTZ) {
   let d = new Date();
-  // console.log(utc);
+  // console.log(Date.now());
   let offsetUTC = d.getTimezoneOffset();
   // console.log(offsetUTC * 60 * 1000);
   d.setMinutes(d.getMinutes() + offsetUTC);
   d.setSeconds(d.getSeconds() + offsetTZ);
-  let hr = d.getHours();
-  let min = d.getMinutes();
-  let sec = d.getSeconds();
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
   //^   rotation of the hands
   let hr_rotation = (hr / 12) * 360 + (min / 60) * 30 + 90;
   let min_rotation = (min / 60) * 360 + (sec / 60) * 6 + 90;
@@ -49,6 +53,178 @@ let minTemp = document.querySelector(`.min-temp`);
 let weatherContainer = document.querySelector(`.weather-container`);
 //* -------------------------------------------------
 
+//* --------- weather and time matrix ---------------
+const weatherMap = new Map();
+const nightMap = new Map();
+const morningMap = new Map();
+const dayMap = new Map();
+const duskMap = new Map();
+
+nightMap.set(
+  `Clear`,
+  `https://media.giphy.com/media/Qx4ltILRcYzHK50B9R/giphy.gif`
+);
+nightMap.set(
+  `Drizzle`,
+  `https://media.giphy.com/media/PbOaO2fedzQLm/giphy.gif`
+);
+nightMap.set(
+  `Clouds`,
+  `https://media.giphy.com/media/3o6Zt93byJYeHqvrwc/giphy.gif`
+);
+nightMap.set(
+  `Rain`,
+  `https://media.giphy.com/media/3ov9jVajUykLpFTMQM/giphy.gif`
+);
+nightMap.set(
+  `Thunderstorm`,
+  `https://media.giphy.com/media/XYxfj2ix1cZwY/giphy.gif`
+);
+nightMap.set(`Snow`, `https://media.giphy.com/media/bGaOdqeqYQKc0/giphy.gif`);
+nightMap.set(`Mist`, `https://media.giphy.com/media/dz6Nrk35xWP3q/giphy.gif`);
+nightMap.set(
+  `Smoke`,
+  `https://media.giphy.com/media/3o7qE7qTCaFrqPAkzC/giphy.gif`
+);
+nightMap.set(
+  `Haze`,
+  `https://media.giphy.com/media/wEM71wbHnFPTHa7FSt/giphy.gif`
+);
+nightMap.set(
+  `Dust`,
+  `https://media.giphy.com/media/WqgM24U3XnnobDPhUx/giphy.gif`
+);
+nightMap.set(`Ash`, `https://media.giphy.com/media/B0FDRYtE2GSS4/giphy.gif`);
+nightMap.set(
+  `Tornado`,
+  `https://media.giphy.com/media/5QXkoDtmD8NGi7MuFP/giphy.gif`
+);
+
+morningMap.set(
+  `Clear`,
+  `https://media.giphy.com/media/jMf1y3bfDMPFA9eFrJ/giphy.gif`
+);
+morningMap.set(
+  `Drizzle`,
+  `https://media.giphy.com/media/t7Qb8655Z1VfBGr5XB/giphy.gif`
+);
+morningMap.set(
+  `Clouds`,
+  `https://media.giphy.com/media/d3PcEfjRsLUqgTGZSE/giphy.gif`
+);
+morningMap.set(
+  `Rain`,
+  `https://media.giphy.com/media/UsMAPgAP1wjW7Coxw2/giphy.gif`
+);
+morningMap.set(
+  `Thunderstorm`,
+  `https://media.giphy.com/media/l0HlLP5fowu9vAbUk/giphy.gif`
+);
+morningMap.set(`Snow`, `https://media.giphy.com/media/PkaZUCdVm2c5G/giphy.gif`);
+morningMap.set(
+  `Mist`,
+  `https://media.giphy.com/media/xT0xeK2kHV7n4qVGEg/giphy.gif`
+);
+morningMap.set(
+  `Smoke`,
+  `https://media.giphy.com/media/XG3vrDXZYW9yg/giphy.gif`
+);
+morningMap.set(
+  `Haze`,
+  `https://media.giphy.com/media/d6JfdOpurCisGQdzdA/giphy.gif`
+);
+morningMap.set(
+  `Dust`,
+  `https://media.giphy.com/media/RoCAThaQXCh9VELk7f/giphy.gif`
+);
+morningMap.set(
+  `Ash`,
+  `https://media.giphy.com/media/JwjLYmdPowBlVMCoZk/giphy.gif`
+);
+morningMap.set(
+  `Tornado`,
+  `https://media.giphy.com/media/rWN1SFLm3g1rQYLHUA/giphy.gif`
+);
+
+dayMap.set(
+  `Clear`,
+  `https://media.giphy.com/media/1Fm7jEapE18HwS6fkT/giphy.gif`
+);
+dayMap.set(
+  `Drizzle`,
+  `https://media.giphy.com/media/3o7at59KDANvJrUDbG/giphy.gif`
+);
+dayMap.set(`Clouds`, `https://media.giphy.com/media/mno6BJfy8USic/giphy.gif`);
+dayMap.set(
+  `Rain`,
+  `https://media.giphy.com/media/3o6wNIK2unphUcCcqQ/giphy.gif`
+);
+dayMap.set(
+  `Thunderstorm`,
+  `https://media.giphy.com/media/3osxYzIQRqN4DOEddC/giphy.gif`
+);
+dayMap.set(`Snow`, `https://media.giphy.com/media/qyCDVJBPdBET6/giphy.gif`);
+dayMap.set(`Mist`, `https://media.giphy.com/media/11uphU5Zfgk1vW/giphy.gif`);
+dayMap.set(`Smoke`, `https://media.giphy.com/media/iXvC4dJHXuNxu/giphy.gif`);
+dayMap.set(
+  `Haze`,
+  `https://media.giphy.com/media/q7Zs2i4boEtq1vPFEE/giphy.gif`
+);
+dayMap.set(
+  `Dust`,
+  `https://media.giphy.com/media/aDeYILgT8f3REd49Sw/giphy.gif`
+);
+dayMap.set(`Ash`, `https://media.giphy.com/media/r5gHt2TCIiHK0/giphy.gif`);
+dayMap.set(`Tornado`, `https://media.giphy.com/media/ed8JGxnQmrke4/giphy.gif`);
+
+duskMap.set(
+  `Clear`,
+  `https://media.giphy.com/media/ZZIEtQHmiTNwuxTOdt/giphy.gif`
+);
+duskMap.set(
+  `Drizzle`,
+  `https://media.giphy.com/media/lQU1BYYxavCPkPiWiT/giphy.gif`
+);
+duskMap.set(`Clouds`, `https://media.giphy.com/media/1uLQUtPLbJMQ0/giphy.gif`);
+duskMap.set(`Rain`, `https://media.giphy.com/media/B2czf5h7JtjNe/giphy.gif`);
+duskMap.set(
+  `Thunderstorm`,
+  `https://media.giphy.com/media/8cdBgACkApvt6/giphy.gif`
+);
+duskMap.set(`Snow`, `https://media.giphy.com/media/bnsWLCG5bEaiI/giphy.gif`);
+duskMap.set(
+  `Mist`,
+  `https://media.giphy.com/media/l0Exhl2a4hQlubRfy/giphy.gif`
+);
+duskMap.set(
+  `Smoke`,
+  `https://media.giphy.com/media/xTiTnlanlqxnyENcZi/giphy.gif`
+);
+duskMap.set(
+  `Haze`,
+  `https://media.giphy.com/media/xT9KVgHIvt5YJXT7W0/giphy.gif`
+);
+duskMap.set(
+  `Dust`,
+  `https://media.giphy.com/media/hFp4sVaHVpipwwMyVa/giphy.gif`
+);
+duskMap.set(
+  `Ash`,
+  `https://media.giphy.com/media/TgXByBtK46u6C6fpco/giphy.gif`
+);
+duskMap.set(
+  `Tornado`,
+  `https://media.giphy.com/media/2TMrbPmlnmyIfZF8KY/giphy.gif`
+);
+
+weatherMap.set(`night`, nightMap);
+weatherMap.set(`morning`, morningMap);
+weatherMap.set(`day`, dayMap);
+weatherMap.set(`dusk`, duskMap);
+
+console.log(weatherMap.get(`morning`).get(`Rain`));
+//* -------------------------------------------------
+
 //* --------------- dom manipulation function -------------------
 
 function dataLogger(obj) {
@@ -60,6 +236,45 @@ function dataLogger(obj) {
   console.log(`The maximum of the day is`, obj.main.temp_max);
 }
 const celsius = "&#8451";
+
+function gifSetter(obj) {
+  let overcontainer = document.querySelector(".overcontainer");
+  const timesOfDay = ["night", "morning", "day", "dusk"];
+  let timeOfDay = timesOfDay[0];
+
+  let sunriseDateObj = new Date(obj.sys.sunrise * 1000);
+  let sunrise = 0;
+  sunriseDateObj.getMinutes() < 30
+    ? (sunrise = sunriseDateObj.getHours())
+    : (sunrise = sunriseDateObj.getHours() + 1);
+  let sunsetDateObj = new Date(obj.sys.sunset * 1000);
+  let sunset = 0;
+  sunsetDateObj.getMinutes() < 30
+    ? (sunset = sunsetDateObj.getHours())
+    : (sunset = sunsetDateObj.getHours() + 1);
+  console.log(sunriseDateObj, sunsetDateObj);
+  console.log(
+    `Sunrise will be: ${sunrise}:00 hours and sunset will be at ${sunset}:00 hours`
+  );
+  console.log(timeOfDay);
+
+  if (hr < sunrise - 1 || hr >= sunset + 1) {
+    timeOfDay = timesOfDay[0];
+  } else if (hr >= sunrise - 1 && hr < sunrise + 1) {
+    timeOfDay = timesOfDay[1];
+  } else if (hr >= sunrise + 1 && hr < sunset - 1) {
+    timeOfDay = timesOfDay[2];
+  } else if (hr >= sunset - 1 && hr < sunset + 1) {
+    timeOfDay = timesOfDay[3];
+  }
+  console.log(timeOfDay);
+  // let background = weatherMap.get(timeOfDay).get(obj.weather[0].main);
+  let background = weatherMap.get(timeOfDay).get(`Rain`);
+
+  overcontainer.style.backgroundImage = `url(${background})`;
+  overcontainer.style.backgroundPosition = "center";
+}
+
 function weatherDisplay(obj) {
   // dataLogger(obj);
   // currentTemp.innerHTML =
@@ -68,17 +283,29 @@ function weatherDisplay(obj) {
   // minTemp.innerHTML = "The day's minimum is " + obj.main.temp_min + " &#8451";
   //? main temperature
   let mainTemp = document.querySelector(`.main-temp`);
-  mainTemp.innerHTML = Math.round(obj.main.temp) + celsius;
 
+  mainTemp.innerHTML =
+    Math.round(obj.main.temp) +
+    celsius +
+    `<p style="font-size: 0.512rem; font-style: italic; justify-item: center">     feels like ${Math.round(
+      obj.main.feels_like
+    )}${celsius}</p>`;
+
+  //? weather description
+  let weatherDescr = document.querySelector(".weather-description");
+  weatherDescr.innerHTML = obj.weather[0].description;
   //? feels like
-  let feelsLike = document.querySelector(".feels-like");
-  feelsLike.innerHTML = `feels like ${Math.round(
-    obj.main.feels_like
-  )}${celsius}`;
+  // let feelsLike = document.querySelector(".feels-like");
+  // feelsLike.innerHTML = `feels like ${Math.round(
+  //   obj.main.feels_like
+  // )}${celsius}`;
 
-  //? min-max temperature div
+  //? location div
+  let icon = obj.weather[0].icon;
   let location = document.querySelector(`.location`);
   location.innerHTML = obj.name;
+  let iconHTML = document.querySelector(".weather-icon");
+  iconHTML.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}.png"> `;
   //? min and max temperature div
   let minTemp = document.querySelector(`.min`);
   let maxTemp = document.querySelector(`.max`);
@@ -88,6 +315,7 @@ function weatherDisplay(obj) {
   clearInterval(timezone);
 
   timezone = setInterval(clock, 1000, obj.timezone);
+  gifSetter(obj);
 }
 
 function getWeather(url) {
